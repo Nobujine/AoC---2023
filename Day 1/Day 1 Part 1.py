740a13368e185c7b8d9f0be7d0819047f9dcf9f7
Eric Collier <ericscollier@gmail.com>
2023-12-01 20:50:50 -0500

    Part 1 works

--- Day 1/Day 1 Part 1.py
+++ Day 1/Day 1 Part 1.py
@@ -1,1 +1,14 @@
-# Hello AoC!
+import re
+
+filename = 'Day 1/input.txt'
+output = 0
+with open(filename, 'r') as f:
+    for line in f.read().splitlines():
+        numbers = re.findall('[0-9]', line)
+        first = numbers[0]
+        last = numbers[-1]
+        number = int(first + last)
+        output += number
+        ...
+
+print(f'sum: {output}')
