05e11ff6e0fac8df3e3682650e618b6f105045d2
Eric Collier <ericscollier@gmail.com>
2023-12-03 10:21:50 -0500

    Day 3 Part 2 Complete

--- Day 3/Day 3 Part 2.py
+++ Day 3/Day 3 Part 2.py
@@ -1,6 +1,7 @@
 import re
+import math
 
-filename = 'Day 3/test_input.txt'
+filename = 'Day 3/input.txt'
 
 def distance(a:tuple, b:tuple):
     return max(abs(a[0]-b[0]), abs(a[1]-b[1]))
@@ -16,12 +17,12 @@ def check_number(positions:tuple, symbols:tuple) -> bool:
 symbols = []
 with open(filename, 'r') as f:
     for y, line in enumerate(f.readlines()):
-        for symbol in re.finditer(r'([^0-9.\n])', line):
+        for symbol in re.finditer(r'([*])', line):
             x = symbol.span()[0]
             symbols.append((x,y,symbol.group()[0]))
 
 # Parse for numbers, check adjacent cells        
-valid_numbers = []
+numbers = []
 with open(filename, 'r') as f:
     for y, line in enumerate(f.readlines()):
         for number in re.finditer('([0-9]+)', line):
@@ -29,7 +30,20 @@ with open(filename, 'r') as f:
             number_text = number.group()
             positions = [(x+i,y) for i in range(len(number_text))]
             if check_number(positions, symbols):
-                valid_numbers.append(int(number_text))
+                numbers.append(((x,y),number_text))
 
-print(f'Sum: {sum(valid_numbers)}')
+# match numbers based on proximity to *
+gear_ratios = []
+for symbol in symbols:
+    gear_numbers = []
+    for number in numbers:
+        positions = [(number[0][0]+i,number[0][1]) for i in range(len(number[1]))]
+        if check_number(positions, [symbol]) == 1:
+            gear_numbers.append(number)
+    if len(gear_numbers) == 2:
+        gear_ratios.append(math.prod([int(i[1]) for i in gear_numbers]))
+    ...
+
+
+print(f'Sum: {sum(gear_ratios)}')
 ...
