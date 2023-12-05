0445fa99842132378c30dca902e04735b99094c9
Eric Collier <ericscollier@gmail.com>
2023-12-05 13:14:31 -0500

    Day 2 part 2 works

--- Day 5/Day 5 Part 2.py
+++ Day 5/Day 5 Part 2.py
@@ -1,4 +1,4 @@
-filename = 'Day 5/test_input.txt'
+filename = 'Day 5/input.txt'
 
 with open(filename, 'r') as f:
     almanac = f.read()
@@ -23,9 +23,9 @@ def convert_two_ranges(input_range, map_range) -> list:
     # wholly covered
     offset = map_range[0] - map_range[1]
     map_range = [map_range[1], map_range[1]+map_range[2]]
-    # Total overlap
+    # Total overlap [10,15], [0,0,25]
     if input_range[0] >= map_range[0] and input_range[1] <= map_range[1]:
-        return [[x+offset for x in input_range]]
+        return [[[x+offset for x in input_range]], []] # [10,15], []
     # 3 <= 4 <= 5
     high_intersection = input_range[0] <= map_range[0] <= input_range[1] 
     low_intersection =  input_range[0] <= map_range[1] <= input_range[1]
@@ -34,33 +34,35 @@ def convert_two_ranges(input_range, map_range) -> list:
         split_range_A = [input_range[0],      map_range[0]-1]
         mapped_range =  [map_range[0]+offset, map_range[1]+offset]
         split_range_B = [map_range[1]+1,        input_range[1]]
-        return [split_range_A, mapped_range, split_range_B] # [0,0], [1,6], [7,10]
+        return [[mapped_range],[split_range_A, split_range_B]] # [1,6], [[0,0], [7,10]]
     # low end intersection [10,20], [0,0,15]
     if low_intersection:
         mapped_range = [input_range[0]+offset, map_range[1]+offset-1]
         split_range =  [map_range[1], input_range[1]]
-        return  [mapped_range, split_range] # [10+,14+], [15,20]
+        return  [[mapped_range], [split_range]] # [10,14], [[15,20]]
     # high end intersection [0,10], [5,5,10]
     if high_intersection:
         split_range = [input_range[0],       map_range[0]-1]
         mapped_range = [map_range[0]+offset, input_range[1]+offset]
-        return [split_range, mapped_range] # [0,4] , [5,10]
+        return [[mapped_range], [split_range]] # [5,10], [[0,4]]
     # no intersection [0,10], [20,20,20]
-    return [input_range] # [0,10]
+    return [[],[input_range]] # [], [[0,10]]
 
 # convert from seed through all the sections
-test = convert_two_ranges([0,10], [1,1,5])
+mapped, not_mapped = convert_two_ranges([57,70], [49,53,8])
 ...
-seed_ranges = [[82,82]]
-for section in sections:
-    converted_ranges = []
-    for i, seed_range in enumerate(seed_ranges.copy()):
-        for map_range in section: 
-            new_ranges = convert_two_ranges(seed_range, map_range)
-            converted_ranges += new_ranges
-    seed_ranges = converted_ranges
-    ...
-low_end = [seed_range[0] for seed_range in seed_ranges]
 
+for section in sections:
+    mapped_ranges = []
+    for map_range in section: 
+        unmapped_ranges = []
+        for seed_range in seed_ranges:
+            mapped, not_mapped = convert_two_ranges(seed_range, map_range)
+            mapped_ranges += mapped
+            unmapped_ranges += not_mapped
+        seed_ranges = unmapped_ranges
+    seed_ranges = mapped_ranges + unmapped_ranges
+
+low_end = [seed_range[0] for seed_range in seed_ranges]
 print(f'Min: {min(low_end)}')
 ...
