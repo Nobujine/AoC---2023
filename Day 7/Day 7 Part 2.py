292641f128e1c88bc6dd0337646aff7678e54d9f
Eric Collier <ericscollier@gmail.com>
2023-12-07 10:24:06 -0500

    simplify code

--- Day 7/Day 7 Part 2.py
+++ Day 7/Day 7 Part 2.py
@@ -1,43 +1,37 @@
 filename = 'Day 7/input.txt'
 
 def determine_score(cards:str) -> int:
-    card_strength = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2','J']
+    card_strength = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
     card_strength.reverse()  # to get increasing scores instead of starting at 0
     unique = ''.join(set(cards))
     count = max([cards.count(u) for u in unique.replace('J',' ')])
-    # ['Q2KJJ', '13', 2111221010]
+
     # Part 2 Adjustment
     if 'J' in cards:
         count += 1 * cards.count('J')
         unique = unique.replace('J','')
-
-    score = 0
-    HAND_MULTIPLIER = 1_000_000_000_000
+    
+    # Determine hand score
     match len(unique):
         case 1 | 0:
-            score = 7 * HAND_MULTIPLIER # Five of a kind or all Joker
+            score = 7 # Five of a kind or all Joker
         case 2:
-            if count == 4:
-                score = 6 * HAND_MULTIPLIER  # Four of a kind
-            elif count == 3:
-                score = 5 * HAND_MULTIPLIER # Full House
+            score = 6 if count == 4 else 5
         case 3:
-            if count == 3:
-                score = 4 * HAND_MULTIPLIER  # Three of a kind
-            elif count == 2:
-                score = 3 * HAND_MULTIPLIER # Two pair
+            score = 4 if count == 3 else 3
         case 4:
-            if count == 2:
-                score = 2 * HAND_MULTIPLIER # One pair
+            score = 2 # One pair
         case 5:
-            score = 1 * HAND_MULTIPLIER  # High card
+            score = 1  # High card
         case _:
             raise ValueError(f'We should not be here! {len(unique)=}')
+
     BASE_TEN_BUFFER = 10  # to ensure that when we concat to a string all numbers take up 2 digits minimum
     card_scores = [str(card_strength.index(c)+BASE_TEN_BUFFER) for c in cards]
+    card_scores.insert(0, str(score+BASE_TEN_BUFFER))  # insert the hand score at the very front
     card_scores = int(''.join(card_scores))
-    score += card_scores
-    return score
+
+    return card_scores
 
 with open(filename, 'r') as f:
     all_hands = f.readlines()
