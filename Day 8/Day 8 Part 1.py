db7a50a3cfb56b0eeab639169d417a6f741121ef
Eric Collier <ericscollier@gmail.com>
2023-12-08 08:53:28 -0500

    test case works

--- Day 8/Day 8 Part 1.py
+++ Day 8/Day 8 Part 1.py
@@ -8,12 +8,15 @@ with open(filename, 'r') as f:
     network = [line.split('=') for line in network]
     network = {start.strip():[c.strip() for c in connection.replace('(', '').replace(')','').strip().split(',')] for start,connection in network}
 
-dir = {'R':1, 'L':0}
+directions = {'R':1, 'L':0}
 # loop until we reach the exit
 i = 0
 current_position = 'AAA'
-while True:
+while current_position != 'ZZZ':
     index = i % len(instructions)
     next_step = instructions[index]
-    current_position = network[next_step]
+    current_position = network[current_position][directions[next_step]]
+    i += 1
+
+print(f'Steps taken: {i}')
 ...
