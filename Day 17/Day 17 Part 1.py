932d5df574559cc53d818ad6698e3c9306ff9595
Eric Collier <ericscollier@gmail.com>
2023-12-19 15:07:11 -0500

    wip

--- Day 17/Day 17 Part 1.py
+++ Day 17/Day 17 Part 1.py
@@ -1,3 +1,5 @@
+
+
 filename = 'Day 17/test_input.txt'
 
 
@@ -20,41 +22,75 @@ class Grid:
     @staticmethod
     def subtract_tuples(A:tuple, B:tuple) -> tuple:
         return tuple(B[i]-A[i] for i in range(min(len(A), len(B))))
-    
-    @staticmethod
-    def L_distance(A:tuple, B:tuple) -> int:
-        return max(B[0]-A[0], B[1]-A[1])
 
 class Node:
     def __init__(self, pos:tuple, value:int) -> None:
         self.pos = pos
         self.value = value
 
+class Cycler:
+    def __init__(self, data:list) -> None:
+        self.data=data
+
+    def get(self, index):
+        return self.data[index%len(self.data)]
+    
+    def locate(self, value) -> int:
+        return self.data.index(value)
+
 def determine_possibilities(path:tuple) -> list:
     UP =    (0, -1)
     DOWN =  (0,  1)
     LEFT =  (-1, 0)
     RIGHT = (1,  0)
-    DIRECTIONS = [RIGHT, DOWN, LEFT, UP]
+    directions = Cycler([RIGHT, DOWN, LEFT, UP])
 
-    if len(path) == 1:  # at start position
-        value = path[0].value
-        return [Node(RIGHT, value), Node(DOWN, value)]
-    if 3 > len(path) > 1: # less than 3 moves 
-        direction = Grid.subtract_tuples(path[-2].pos,path[-1].pos)
-    if len(path) >= 3:  # more than 3 moves, start checking 
-        ...
+    # at start position
+    if len(path) == 1:  
+        return [RIGHT, DOWN]
+
+    # for any other position
+    direction = Grid.subtract_tuples(path[-2].pos,path[-1].pos)
+    index = directions.locate(direction)
+    new_directions = [index-1, index+1, index]
+    if len(path) >= 3:
+        if check_straight_line(path):
+            new_directions.remove(index)
+    new_directions = [directions.get(index) for index in new_directions]
+    return new_directions
+
+def check_straight_line(path:list)->bool:
+    # returns true if the last 3 steps were in a straight line
+    directions = set(Grid.subtract_tuples(path[-1-i].pos,path[-i].pos) for i in range(1,3))
+    if len(directions) == 1:
+        return True
+    return False
     ...
+
+def score(node:Node, end:tuple) -> int:
+    return node.value + max(end[0]-node.pos[0], end[1]-node.pos[1])
 
 def pathfind(grid:Grid, start:Node, end:tuple) -> list:
     # returns a list of node objects
-    path = []
-    path.append(start)
-    path.append(Node((0,1),1))
-    path.append(Node((0,2),2))
+    open_list = [[start]]
+    closed_list = []
 
-    test = determine_possibilities(path)
-    ...
+    while open_list:
+        open_list.sort(key=lambda x:score(x[-1], end))
+        path = open_list.pop(0)
+        if path[-1].pos == end:
+            return path
+        closed_list.append(path)
+        new_dirs = determine_possibilities(path)
+        for direction in new_dirs:
+            pos = grid.add_tuples(path[-1].pos, direction)
+            if grid.inside_grid(pos):
+                value = grid.tiles[pos] + path[-1].value
+                new_path = path.copy()
+                new_path.append(Node(pos, value))
+                if new_path not in closed_list:
+                    open_list.append(new_path)
+        ...
 
 def main():
     # Read file input
@@ -66,7 +102,8 @@ def main():
                 grid.tiles[pos] = int(value)
     grid.size = (x,y)
 
-    start = Node((0,0), 0)
+    start_pos = (0,0)
+    start = Node(start_pos, grid.tiles[start_pos])
     end = grid.size  # bottom right corner, per Da Rules
     path = pathfind(grid, start, end)
     ...
